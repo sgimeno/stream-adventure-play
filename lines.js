89e2e28b59fb872ebfcce026f01c4f1d5f6a70a5
Sergio Gimeno <gimeno.sergio@gmail.com>
2016-05-21 12:17:51 +0200

    lines

--- lines.js
+++ lines.js
@@ -1,8 +1,27 @@
-var split = require('split');
-    process.stdin
-            .pipe(split())
-          .pipe(through2(function (line, _, next) {
-                        console.dir(line.toString());
-                                    next();
-                                            }))
-    ;
+'use strict';
+const through = require('through2');
+const split = require('split');
+
+let currentLine = 0;
+
+function isOdd(n) {
+   return Math.abs(n % 2) == 1;
+}
+
+process.stdin
+  .pipe(split())
+  .pipe(through(function (line, _, next) {
+
+    if (isOdd(currentLine)) {
+      this.push(line.toString().toUpperCase() + '\n');
+    } else {
+      this.push(line.toString().toLowerCase() + '\n');
+    }
+
+    currentLine++;
+
+    next();
+  }))
+  .pipe(process.stdout)
+
+  ;
